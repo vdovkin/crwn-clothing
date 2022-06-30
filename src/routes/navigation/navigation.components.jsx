4c7ea6ee27277834c89ae1a003ccf18e0e36bd19
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2022-06-30 22:33:55 +0300

    add cart items adn shpo componetn with products context

--- src/routes/navigation/navigation.components.jsx
+++ src/routes/navigation/navigation.components.jsx
@@ -1,11 +1,14 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -29,7 +32,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
